24a04a500d380c5083bae43af4a75af113b3d581
harmel3056 <melh1988@hotmail.com>
2025-09-03 16:20:42 +1000

    Refactor due to multiple loops in original code

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-ranges = (range(97, 101), range(102, 113), range(114, 123))
-output = [num for i in ranges for num in i]
-ascii_output = ''.join(chr(num) for num in output)
-print(f"{ascii_output}", end="")
+for i in range(97,123):
+    if i != 101 and i != 113:
+         print("{}".format(chr(i)), end="")
