0f30f710eb4ba5db12aec2405e936cf6e129e503
harmel3056 <melh1988@hotmail.com>
2025-09-04 20:02:07 +1000

    Imports a function for use in the current function/program

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    def add(a, b):
+    import add_0 as add_func
+
+    a = 1
+    b = 2
+    c = add_func.add(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
         
