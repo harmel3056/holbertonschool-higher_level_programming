92c13e82a0edb067450f65f69a4499224755e6e0
harmel3056 <melh1988@hotmail.com>
2025-09-05 07:40:23 +1000

    Takes a list of arguments and formats them in to a numbered list with a tally

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import
+def print_args(*argv):
+    if len(argv) == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(argv)), end="\n")
+        j = 1
+        for i in range(len(argv)):
+            print("{}: {}".format(j, argv[i]), end="\n")
+            j += 1
