09c77a974f387e7a369135dc055658eb45bf3ef6
harmel3056 <melh1988@hotmail.com>
2025-09-05 08:10:01 +1000

    Amended to take command-line arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_args(*argv):
-        if len(argv) == 0:
-            print("0 arguments.")
+import sys
+
+
+def print_args(*argv):
+    if len(argv) == 0:
+        print("0 arguments.")
+    else:
+        if len(argv) == 1:
+            print("1 argument:")
         else:
             print("{} arguments:".format(len(argv)), end="\n")
-            j = 1
-            for i in range(len(argv)):
-                print("{}: {}".format(j, argv[i]), end="\n")
-                j += 1
+        j = 1
+        for i in range(len(argv)):
+            print("{}: {}".format(j, argv[i]), end="\n")
+            j += 1
+
+
+if __name__ == "__main__":
+    print_args(sys.argv[1:])
