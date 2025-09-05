b82d8eaef0adb63200a4c029822218db043a3aa1
harmel3056 <melh1988@hotmail.com>
2025-09-05 10:52:36 +1000

    Working on a style that is adaptable as a standalone function as well as for use with command line

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 import sys
 
 
-def add_args(argv):
+def add_args(*argv):
     int_args = [int(arg) for arg in argv]
     print(sum(int_args))
 
 
 if __name__ == "__main__":
-    print(*sys.argv[1:])
+    add_args(*sys.argv[1:])
