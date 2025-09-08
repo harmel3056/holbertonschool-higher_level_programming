e329065034119778994c148dbc54ed6ec3261d1c
harmel3056 <melh1988@hotmail.com>
2025-09-09 07:06:47 +1000

    Gives the squared value of matrix ints and returns them using .append to reconstructthe matrix format

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-     result = []
-     for row in matrix:
+    result = []
+    for row in matrix:
         row_dupe = list(row)
+        squared_row = []
         for element in row_dupe:
-            multiple = (element ** 2)
-            result += [multiple]
-        return result
+            squared_row.append(element ** 2)
+        result.append(squared_row)
+    return result
