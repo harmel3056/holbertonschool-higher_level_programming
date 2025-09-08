a3c036082013fe4b2b6b48f7a7fa3135c8715a89
harmel3056 <melh1988@hotmail.com>
2025-09-08 14:44:53 +1000

    Solve for empty string

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_value = my_list[0]
-
-    if my_list == "":
+    if not my_list:
         return "None"
-    else:
-        for digit in my_list:
-            if digit > max_value:
-                max_value = digit
+
+    max_value = my_list[0]
+    for digit in my_list:
+        if digit > max_value:
+            max_value = digit
 
     return max_value
