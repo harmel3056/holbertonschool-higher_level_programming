01309e84c75834d9d6499c509b1073f9ac43a5c1
harmel3056 <melh1988@hotmail.com>
2025-09-08 10:46:09 +1000

    Explicit :d formatting required to specify decimal integer for checker

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
 
 # could do this without .format sans-constraints
 #    for item in my_list:
