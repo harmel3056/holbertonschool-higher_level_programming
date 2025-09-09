97b78436bbb108a6d8acc6cb33e09c0d07a1a032
harmel3056 <melh1988@hotmail.com>
2025-09-09 15:20:20 +1000

    Returns the int value for roman numerals entered, using dictionary mapping

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_dictionary = {
+        "I": 1,
+        "II": 2,
+        "III": 3,
+        "IV": 4,
+        "V": 5,
+        "VI": 6,
+        "VII": 7,
+        "VIII": 8,
+        "IX": 9,
+        "X": 10,
+        "XI": 11,
+        "XII": 12,
+        "XIII": 13,
+        "XIV": 14,
+        "XV": 15,
+        "XVI": 16,
+        "XVII": 17,
+        "XVIII": 18,
+        "XIX": 19,
+        "XX": 20,
+        "L": 50,
+        "LXXXVII": 87,
+        "C": 100,
+        "D": 500,
+        "DCCVII": 707,
+        "M": 1000
+    }
+
+    for key in roman_dictionary:
+        if roman_string == key:
+            return roman_dictionary[key]
