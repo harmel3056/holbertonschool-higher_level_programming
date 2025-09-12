ad15ea1b323903fd9392e3a9954d9f9502db01bb
harmel3056 <melh1988@hotmail.com>
2025-09-12 14:08:15 +1000

    Test and Function to print a square comprising hashes

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+"""
+
+
+def print_square(size):
+    """
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    column = []
+    for qty in range(size):
+        print("#" * size)
+    return
