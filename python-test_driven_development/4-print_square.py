e6d0a7c9927c7382149904088f0cc2728d16344e
harmel3056 <melh1988@hotmail.com>
2025-09-12 14:16:02 +1000

    Amended with documentation and one additional test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 
 """
+This module provides a function that will print
+a square of hashes
+
+print_square validates the input and then
+prints out the same number of hashes in rows and columns
 """
 
 
 def print_square(size):
     """
+    Prints a square of hashes
+
+    Args:
+        size: number of columns and rows of hashes to print
+    Returns:
+        a printed square of hashes
+    Raises:
+        TypeError for incorrect entries
+        ValueError for numbers entered below 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
