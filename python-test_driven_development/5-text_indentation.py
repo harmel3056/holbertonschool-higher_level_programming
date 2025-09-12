f648a67b8f36e5e3b82548db43432a4012d09840
harmel3056 <melh1988@hotmail.com>
2025-09-12 15:23:48 +1000

    Test and function for reformatting of entered text

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module provides a function that prints a reformatted
+version of the string that is entered
+"""
+
+
+def text_indentation(text):
+    """
+    Reformats a string using the ?.: denominators
+
+    Args:
+        text: a string
+    Returns:
+        string printed with newlines whereever ?.: occur
+    Raises:
+        TypeError for incorrect entries
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    new_text = ""
+    skip_space = False
+
+    for char in text:
+        if skip_space and char == " ":
+            continue
+
+        skip_space = False
+
+        new_text += char
+        if char in ".?:":
+            new_text += "\n\n"
+            skip_space = True
+
+    print(new_text.strip())
