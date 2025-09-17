fbf1ef5d2b77807b61d239348b1cc759b4c81971
harmel3056 <melh1988@hotmail.com>
2025-09-17 12:27:31 +1000

    Defining a private instance attribute for the Square class created in 0-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a private instance attribute for Square class"""
+class Square:
+    """
+    Defines a private instance attribute for Square class
+    
+    Args:
+    None
+
+    Return:
+    Nada
+    """
+    def __init__(self, size):
+        self.__size = size
+        
