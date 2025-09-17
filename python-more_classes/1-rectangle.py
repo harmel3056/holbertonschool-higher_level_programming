b55664efa1be51a105d6eb173d604fe5da8f899f
harmel3056 <melh1988@hotmail.com>
2025-09-17 21:16:08 +1000

    Definition of a rectangle class with input values

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Defines the Rectangle class in greater detail
+"""
+
+
+class Rectangle:
+    """
+    A definition of the rectangle including input values
+    and entry limitations
+
+    Args:
+    width - defines the width of the rectangle as an int
+    height - defines the height of the rectangle as an int
+
+    Return:
+    None
+    """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not (isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not (isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
