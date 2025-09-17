e03eab415f6728c1ee1e794c8d79201ec6824178
harmel3056 <melh1988@hotmail.com>
2025-09-17 12:07:57 +1000

    File setup and first task to create an empty class

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defining an empty class called Square
+"""
+class Square:
+    """
+    Defines a class called Square only
+
+    Args:
+    None
+
+    Returns:
+    Nada
+    """
+    pass
