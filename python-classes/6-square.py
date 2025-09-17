5342a79b47454580e6f874ba0b3cf517930cd443
harmel3056 <melh1988@hotmail.com>
2025-09-17 18:54:47 +1000

    Fix to resolve issue with exceptions not being raised

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,7 +21,7 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
@@ -41,7 +41,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, int) and len(value[1]) > 0:
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(v, int) and v >= 0 for v in value)
+                ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
