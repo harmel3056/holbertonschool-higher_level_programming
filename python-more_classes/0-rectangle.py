ed66a4d5cc9cb43540391e4bbecec7a34e4765c7
harmel3056 <melh1988@hotmail.com>
2025-09-17 20:53:53 +1000

    File setup and initialise first task

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines an empty class called Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Empty class called Rectangle
+
+    Args:
+    None
+
+    Return:
+    None
+    """
+    pass
