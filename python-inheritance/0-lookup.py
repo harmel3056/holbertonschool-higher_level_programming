39afbbea39eead0ce6e994b3aa506a38cd3bb506
harmel3056 <melh1988@hotmail.com>
2025-09-22 11:38:55 +1000

    Returns a directory of the available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -5,4 +5,13 @@ lookup returns a list of attributes and methods of an object
 
 
 def lookup(obj):
+    """
+    returns a list of attributes and methods of an object
+
+    Args:
+    object - object whose attributes will be returned
+
+    Return:
+    list of attributes and methods of an object
+    """
     return dir(obj)
