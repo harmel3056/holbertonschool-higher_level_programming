e12a9f7f6556286210f2445b4cb4f6c85703bd76
harmel3056 <melh1988@hotmail.com>
2025-09-22 20:58:34 +1000

    Rectangle class with validation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
+New class Rectangle which inherits from BaseGeometry and uses its
+function integer_validator to validate its input
 """
 
 
 class Rectangle(BaseGeometry):
     """
+    Inherits from BaseGeometry to validate width and height, and
+    then assigns their values
+
+    Args:
+    BaseGeometry - a separate class which aids in validation
+
+    Return:
+    None
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
