00b01a7d5f5d65bb29cad02ab21afa9db59c5f34
harmel3056 <melh1988@hotmail.com>
2025-09-22 15:37:17 +1000

    Function to identify whether object is part of a class family

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """
+Returns True for an object that is either the type identified
+or is an inherited instance of the type identified
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
+    Identifies if an object is part of a class family
+
+    Args:
+    obj - object to identify type family
+
+    Return:
+    True if object is an instance of, or if the object
+    is an instance of a class that inherited from, the
+    specified class. Otherwise False.
     """
     if (isinstance(obj, a_class)):
         return True
