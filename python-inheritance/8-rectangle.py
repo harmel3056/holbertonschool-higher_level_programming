989a81ccf7c50265fc6f5bf4297bcf5c78c57983
harmel3056 <melh1988@hotmail.com>
2025-09-22 21:04:01 +1000

    Update documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 New class Rectangle which inherits from BaseGeometry and uses its
 function integer_validator to validate its input
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
