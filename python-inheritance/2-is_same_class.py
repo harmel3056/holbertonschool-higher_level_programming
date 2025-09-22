b2072bb3446c6c12c6beaf18e23af7c4b10620e5
harmel3056 <melh1988@hotmail.com>
2025-09-22 15:25:44 +1000

    Function that returns whether an object is an exact match for the type given, regardless of inheritance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Specifically identifies if an object is of the same type
+as that specified. Inheritance is disregarded.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns whether an object is the same type specified
+
+    Input:
+    obj - object to check type of
+    a_class - type to match
+
+    Return:
+    True for an exact match
+    False where exact match not found
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
