be67d61b818ad3cd060d84c30619b6fbb05ae648
harmel3056 <melh1988@hotmail.com>
2025-09-23 10:42:50 +1000

    notation update

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -21,3 +21,9 @@ def is_kind_of_class(obj, a_class):
         return True
     else:
         return False
+
+# Note that this is a super C way of writing this:
+# isinstance already returns a boolean (True or False) so you
+# could simply have passed:
+# return isinstance(obj, a_class)
+# into the function
