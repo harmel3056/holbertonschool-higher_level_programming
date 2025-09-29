9176d9752b8e34d9270adc4ce565e9492c8e0aa4
harmel3056 <melh1988@hotmail.com>
2025-09-29 16:05:46 +1000

    Function to read a text file and print it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Reads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout
+
+    Args:
+    name of the file to read
+
+    Return:
+    Prints the contents of the file as a string
+    """
+    with open(filename, "r", encoding="UTF-8") as f:
+        data = f.read()
+    print(data)
+
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
