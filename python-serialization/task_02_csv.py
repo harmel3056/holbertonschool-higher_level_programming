c0be429be3716228848a3f100b06937fdb952ffc
harmel3056 <melh1988@hotmail.com>
2025-10-04 10:02:25 +1000

    Converts a file from CSV format to JSON

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """
+Converts CSV format to JSON using DictReader
 """
 import csv
 import json
 
 
 def convert_csv_to_json(filename):
+    """
+    Converting CSV format to JSON using DictReader
+
+    Args:
+    filename - name of the file to run conversion on
+
+    Return:
+    True if conversion is successful, False if exception raised
+    """
     try:
         with open(filename, "r", encoding="UTF-8") as f:
-#            csv_reader = csv.reader(f)
             dictionary = csv.DictReader(f)
             for row in dictionary:
-                write(json.dumps(row))
+                f.write(json.dumps(row))
             return True
-    except (FileNotFoundError, EOFError):
+    except Exception:
         return False
 
 
