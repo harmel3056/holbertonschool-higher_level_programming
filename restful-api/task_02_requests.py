1692629ebbed8335e5d53128f8ac2670ece0cd51
harmel3056 <melh1988@hotmail.com>
2025-10-09 07:50:35 +1100

    Exploring post retrieval methods

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,25 +1,46 @@
 #!/usr/bin/python3
 """
+Exploring post retrieval methods using requests
 """
 import requests
+import csv
 
 def fetch_and_print_posts():
-    url = "https://jsonplaceholder.typicode.com/posts/1"
+    """
+    Retrieves posts and prints the status code followed
+    by a list of titles
+    """
+    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    
-    data = response.json()
-    print(data)
 
-    # api_url = "https://jsonplaceholder.typicode.com/posts/1"
-    # response = requests.get(api_url)
+    print(f"Status code: {response.status_code}")
 
-    # # Check if the request was successful
-    # if response.status_code == 200:
-    #     # Print the JSON content of the response
-    #     data = response.json()
-    #     print("Successfully retrieved data:")
-    #     print(data)
-    # else:
-    #     print(f"Failed to retrieve data. Status code: {response.status_code}")
 
-fetch_and_print_posts()
+    if response.status_code == 200:
+        data = response.json()
+
+        for title in [post['title'] for post in data]:
+            print(title)
+#fetch_and_save_posts():
+
+
+def fetch_and_print_posts():
+    """
+    Retrieves posts and converts them to CSV format
+    """
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        rows = [
+            {'id': post['id'], 'title': post['title'], 'userId': post['userId']}
+            for post in data
+        ]
+
+        with open("posts.csv", "w", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "userId"])
+            writer.writeheader()
+            writer.writerows(rows)
+#fetch_and_print_posts()
