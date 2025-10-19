0d66181b5eb99295789edbce96753414c65a9932
harmel3056 <melh1988@hotmail.com>
2025-10-19 20:03:07 +0000

    Creates a MySQL user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Creates a MySQL user if it doesn't yet exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION; 
