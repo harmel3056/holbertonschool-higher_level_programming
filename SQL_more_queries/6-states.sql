e424b4f73bfc45b5aa118635854a93fb9d4cce08
harmel3056 <melh1988@hotmail.com>
2025-10-19 21:29:02 +0000

    Creates a database and a table with a primary key

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates a database and a table with a primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);
