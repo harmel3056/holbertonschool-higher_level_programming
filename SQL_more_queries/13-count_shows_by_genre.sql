ae4352c431a0f385d571d029f8c90fa8b6684e59
harmel3056 <melh1988@hotmail.com>
2025-10-23 03:59:29 +0000

    Utilises an inner join to select aliased, grouped attributes from a database, and then counts them

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
--- Utilises an inner join to select attributes from a database, and then counts them
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
+-- Utilises an inner join to select aliased, grouped attributes from a database, and then counts them
+SELECT tv_show_genres.tv_genres AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
