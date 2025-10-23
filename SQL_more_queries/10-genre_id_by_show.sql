3c8a7819bc9cc2a113077b93776a4bc348b94f18
harmel3056 <melh1988@hotmail.com>
2025-10-23 02:54:27 +0000

    Corrected genre tables column title

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
--- Lists items that have at least one attribute match
+-- Lists database items that have at least one attribute match
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title DESC;
