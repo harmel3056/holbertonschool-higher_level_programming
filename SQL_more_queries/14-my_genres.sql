8a847327229cfe70a360199589028368bd32227f
harmel3056 <melh1988@hotmail.com>
2025-10-23 21:12:15 +0000

    Uses two inner joins and WHERE function to obtain attributes list

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
---
+-- Uses two inner joins and WHERE function to obtain attributes list
 SELECT tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id 
-WHERE tv_shows.title = 'Dexter'
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
