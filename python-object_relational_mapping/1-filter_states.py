aa430b9da300f61808b3f4cf3f8c41520400ee64
harmel3056 <melh1988@hotmail.com>
2025-10-24 23:08:45 +0000

    Filters database results for names starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Filters database results for names starting with N
+Filters database results for names starting with N by:
+Taking 3 arguments: MySQL
 """
 import MySQLdb
 import sys
@@ -12,19 +13,19 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute(
-    "SELECT * FROM states "
-    # LEFT targets leftmost chars of desired qty
-    "WHERE LEFT(name, 1) = %s "
-    "ORDER BY id ASC"
-    )
-cursor.execute(query, ('N',))
+    cursor.execute(
+        "SELECT * FROM states "
+        # LEFT targets leftmost chars of desired qty
+        "WHERE LEFT(name, 1) = %s "
+        "ORDER BY id ASC"
+        )
+    cursor.execute(query, ('N',))
 
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
