cbb0ed39e9f8715b7fcaf68fae3da31afb3c3e7a
harmel3056 <melh1988@hotmail.com>
2025-10-25 00:37:06 +0000

    Extracts values matching a name argument passed that is safe from MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Extracts values matching a name argument passed
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    query = """
+        SELECT * FROM states
+        WHERE name = %s
+        ORDER BY id ASC
+        """
+    cursor.execute(query, (state_name_searched,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
