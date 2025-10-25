a8b217737971f714d54253a4a46117163f3aaebb
harmel3056 <melh1988@hotmail.com>
2025-10-25 03:33:22 +0000

    Establishes a class definition for State

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Establishes a class definition for State
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    # nullable=False not necessary for PRIMARY KEYS
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
