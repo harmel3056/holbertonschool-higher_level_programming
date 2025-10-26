ab6dd1fe817eb34f010e78fffe5e48c7dc46b17c
harmel3056 <melh1988@hotmail.com>
2025-10-26 20:28:48 +1100

    Uses SQLAlchemy to extract objects matching the command line entry

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     session = Session()
 
     state = session.query(State).filter(
-        State.name.contains(sys.argv[4])).order_by(State.id).all()
+        State.name == sys.argv[4]).order_by(State.id).all()
     if state:
         for row in state:
             print(f"{row.id}")
