8190b31627389ce50b6b292b325d17ccdaca989e
harmel3056 <melh1988@hotmail.com>
2025-10-26 20:12:19 +1100

    Uses SQLAlchemy to extract objects matching the command line entry

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Uses SQLAlchemy to extract objects matching the command line entry
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{passwd}@localhost:3306/{db_name}"
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(
+        State.name.contains(sys.argv[4])).order_by(State.id).all()
+    for row in state:
+        print(f"{row.id}: {row.name}")
+
+    session.close()
