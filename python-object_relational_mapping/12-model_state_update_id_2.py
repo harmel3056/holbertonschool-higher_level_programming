3c3399a86d67857bcbd3b4216b4915ed4afc37bb
harmel3056 <melh1988@hotmail.com>
2025-10-26 21:13:57 +1100

    Uses SQLAlchemy to update an object in a database

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Uses SQLAlchemy to insert a state into a database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{passwd}@localhost:3306/{db_name}"
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
