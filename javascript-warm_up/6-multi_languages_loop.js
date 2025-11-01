f46d0e41b8e7ed11faed790fbee455ee7b31b05a
harmel3056 <melh1988@hotmail.com>
2025-11-01 23:36:36 +0000

    Prints three lines by using an array of strings and a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Prints three lines by using an array of strings and a loop
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const item of array) {
+  console.log(item);
+}
