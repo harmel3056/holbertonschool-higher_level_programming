50f4e30428f89c8353a095f1beb6c2e6d41b1ba5
harmel3056 <melh1988@hotmail.com>
2025-11-02 00:48:03 +0000

    Prints a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Prints a square
+
+const size = Number(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} for (let i = 0; i < size; i++) {
+  let row = '';
+  for (let j = 0; j < size; j++) {
+    row += 'X';
+  }
+  console.log(row);
+}
